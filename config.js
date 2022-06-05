0816b581ab108b2a57e964e84d82f4bd56bf33dd
TipengOfc <100071150+ipang22222@users.noreply.github.com>
2022-06-05 19:46:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,12 +3,12 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 
-global.owner = ['6281220670449','0']
-global.packname = 'Bot Whatsapp'
+global.owner = ['6282136031499','0']
+global.packname = 'Bot Tipeng'
 global.author = 'TRASH'
 global.sessionName = 'data'
 global.prefa = ['','!','.','#']
-global.ownername = 'KurrXd'
+global.ownername = 'TIPENG HOST'
 global.sp = '>'
 global.mess = {
     admin: 'Fitur Khusus Admin Group!',
